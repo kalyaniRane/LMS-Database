05cd50b1d8b4416602739b2a3598ce0aec7bb45b
kalyaniRane <kalyanirane19@gmail.com>
2020-08-09 12:27:45 +0530

    ADDED : Query To Create A User Registration Table

--- DDL.sql
+++ DDL.sql
@@ -1,3 +1,15 @@
 create database lms;
 
 use lms;
+
+create table userDetails(
+id int primary key,
+email varchar (200),
+first_name varchar (200),
+last_name varchar (200),
+password varchar (200),
+contact_number varchar (10),
+verified boolean,
+creator_stamp DATETIME,
+creator_user varchar (200)
+);
