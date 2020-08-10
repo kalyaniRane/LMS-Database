e182c26b7cdd03ca6d051b1ca0ac2c16c0b06d4b
kalyaniRane <kalyanirane19@gmail.com>
2020-08-10 23:26:47 +0530

    ADDED : Query To Insert Data Into Fellowship Candidate Table

--- DML.sql
+++ DML.sql
@@ -22,3 +22,11 @@ INSERT INTO `hiredcandidate` VALUES
 (8,'Afreen','Sadiq','Mehra','mehra.afreen@gmail.com','Pune','MCA','2020-07-06','9856788912',400002,'Mumbai','Good','Good','Good','Good','A','2020-07-07','Kalyani'),
 (9,'Aftab','Ilyas','Shaikh','affy@gmail.com','Mumbai','MCA','2020-07-07','8655292685',400003,'Pune','Good','Good','Good','Good','A','2020-07-07','Kalyani'),
 (10,'Izhaan','Istiyak','Khan','izhaan@gmail.com','Pune','MCA','2020-07-08','8286548410',400002,'Pune','Good','Good','Good','Good','A','2020-07-08','Kalyani');
+
+INSERT INTO `fellowship_candidate` VALUES
+(1,'kalyani','sandesh','rane','kalyanirane@gmail.com','mumbai','MCA','2020-01-03','7586324189','425001','Mumbai','Good','Good','Good','Good','2020-07-03','Kalyani','1996-05-21',1,'sandesh','Business',8569742310,100000,'Jalgaon','Jalgoan','jcjsd/cjds','2020-01-03','joined','Done','Done','Done',1,'Good'),
+(2,'Zaid','Istiyak','Ansari','zaid@gmail.com','Pune','BE Com','2020-06-25','8956237411',400102,'Pune','Good','Good','Good','Good','2020-07-03','Kalyani','1995-03-25',1,'Istiyak','Business',8695423170,1000000,'Mumbai','Jalgoan','jcjsd/cjds/cdfg','2020-01-03','joined','Done','Done','Done',1,'Good'),
+(3,'Pratiksha','Kadam','Deshmukh','pratiksha@gmail.com','Mumbai','BE Com','2020-07-01','9632587410',400088,'Mumbai','Good','Excellent','Excellent','Good','2020-07-02','Kalyani','1994-06-14',1,'Kadam','Job',9654123780,300000,'Pune','Pune','ghry/nff/huiu','2020-03-25','joined','Done','Pending','Done',0,'Good'),
+(4,'Pritam','Laxman','Suryawanshi','pritam@gmail.com','Mumbai','BE Com','2020-07-05','9878529630',400001,'Banglore','Good','Good','Good','Good','2020-07-07','Kalyani','1996-12-12',1,'Laxman','Business','9874510147',150000,'sfdgs','shgdh','dhs/sg/dgdh','2019-12-20','joined','done','done','Done',1,'good'),
+(5,'Junaid','Istiyak','Ansari','junaid@gmail.com','Mumbai','BE Com','2020-05-29','9874563210',400043,'Mumbai','Good','Good','Good','Good','2020-07-01','Kalyani','1996-08-12',1,'Istiyak','Business','9874561230',100000,'abcd','abcd','abc/xyz','2020-07-15','joined','done','done','Done',1,'good'),
+(6,'Izhaan','Istiyak','Khan','izhaan@gmail.com','Mumbai','MCA','2020-06-30','9856223741',400043,'Mumbai','Good','Good','Good','Good','2020-07-01','Kalyani','1996-12-20',1,'Istiyak','Business','7894561237',100000,'wxyz','wxyz','pqr/qwe','2020-06-01','joined','done','done','Done',0,'good');
