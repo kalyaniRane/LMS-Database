576c4aaff8b76a356ec7bfdeac5e2b59388e7b16
kalyaniRane <kalyanirane19@gmail.com>
2020-08-10 21:59:38 +0530

    ADDED : Query To Insert Data Into User Details Table

---
+++ DML.sql
@@ -0,0 +1,12 @@
+INSERT INTO `userdetails` VALUES 
+(1,'kalyanirane@gmail.com','Kalyani','Rane','kalyani@123','8956231047',0,'2020-05-07','kalyani'),
+(2,'zaid@gmail.com','Zaid','Ansari','zaq12@az','8974561232',0,'2020-02-20','kalyani'),
+(3,'izhaan54@gmail.com','Izhaan','Khan','izer3@qw','9876543210',1,'2020-03-05','kalyani'),
+(4,'poojarao@gmail.com','Pooja','Rao','zxy1@az','8912056123',1,'2020-03-16','kalyani'),
+(5,'pratiksha45@gmail.com','Pratiksha','Deshmukh','zx1@abc','8912056124',1,'2020-03-29','kalyani'),
+(6,'sumit@gmail.com','Sumit','Gohil','azsd@1a','7862056124',1,'2020-04-01','kalyani'),
+(7,'mehra.afreen@gmail.com','Afreen','Mehra','aznm@1a','7862457889',0,'2020-04-18','kalyani'),
+(8,'affy@gmail.com','Aftab','Shaikh','mnbv1@q','9963524178',0,'2020-04-26','kalyani'),
+(9,'gajanan@gmail.com','Gajanan','Desai','gaja@123','9638527410',1,'2020-04-30','kalyani'),
+(10,'junaid@gmail.com','Junaid','Ansari','abc@ac12','7894561231',1,'2020-02-12','kalyani'),
+(11,'pritam@gmail.com','Pritam','Suryawanshi','surya@1','8952827110',0,'2020-05-14','kalyani');
